83896e56f44ec8fa643bd3de5ad582c79a7bc05b
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-09-15 14:16:11 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -22,15 +22,3 @@ export default class MovieShowcase extends Component {
     )
   }
 }
-
-
-// export default class MovieShowcase extends Component {
-// 	generateMovieCards = () => {
-// 		return movieData.map((movie, index) => <MovieCard
-// 		      key={index}
-// 		      title={movie.title}
-// 		      IMDBRating={movie.IMDBRating}
-// 		      genres={movie.genres}
-// 		      poster={movie.poster}
-// 		    />)
-// 	};
